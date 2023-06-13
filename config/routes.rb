5d7efb33df0bbb61fcb4b4c5d434c59a225538d1
Yash karray <yashkarray14@gmail.com>
2023-06-13 22:52:21 +0530

    added routes for the sso saml authentication

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,4 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
   post 'abs/saml/consume', to: "saml#consume"
   get 'saml/initiate', to: "saml#initiate"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
